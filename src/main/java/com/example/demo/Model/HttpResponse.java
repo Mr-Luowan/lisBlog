863ed57bf1bcf14bac89fbd4d539fe5ef876d0d8
lipengfei <lipengfei6@vishee.com>
2023-01-05 17:50:49 +0800

    格式错误修改

--- src/main/java/com/example/demo/Model/HttpResponse.java
+++ src/main/java/com/example/demo/Model/HttpResponse.java
@@ -10,18 +10,45 @@ public class HttpResponse {
     private Object data;
     private String message;
 
+    private HttpResponse(int status, Object data, String message) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     public static HttpResponse success() {
-        HttpResponse httpResponse = new HttpResponse(200, null, "ok");
-        return httpResponse;
+        return new HttpResponse(200, null, "ok");
     }
 
     public static HttpResponse success(Object object) {
-        HttpResponse httpResponse = new HttpResponse(200, object, "ok");
-        return httpResponse;
+        return new HttpResponse(200, object, "ok");
     }
 
     public static HttpResponse error() {
-        HttpResponse httpResponse = new HttpResponse(400, null, "error");
-        return httpResponse;
+        return new HttpResponse(400, null, "error");
     }
 }
